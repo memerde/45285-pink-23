34fc73987cc07064187d0689fbfbd7e4b87ba83d
mrd <three.two@ya.ru>
2021-09-08 18:15:49 +0300

    5-2

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,16 @@
 var header = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__close');
 var navigation = document.querySelector('.navigation');
+var menuButton = document.querySelector('.page-header__close');
+var cta = document.querySelector('.container__cta--reset-tablet');
 
 header.classList.remove('page-header--nojs');
 header.classList.remove('page-header--no-opacity');
-navigation.classList.add('navigation--toggle')
-
+navigation.classList.add('navigation--toggle');
+menuButton.classList.remove('page-header__close--nojs');
+if (cta) {cta.classList.remove('container__cta--reset-tablet--nojs');}
 
 navToggle.addEventListener('click', function () {
-  navigation.classList.toggle('navigation--toggle')
-  header.classList.toggle('page-header--no-opacity')
+  navigation.classList.toggle('navigation--toggle');
+  header.classList.toggle('page-header--no-opacity');
 });
