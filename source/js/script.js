379a967898a4b212fd38cad83f2cb6b817007291
mrd <three.two@ya.ru>
2021-09-09 14:15:11 +0300

    6-1

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,13 @@ var header = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__close');
 var navigation = document.querySelector('.navigation');
 var menuButton = document.querySelector('.page-header__close');
-var cta = document.querySelector('.container__cta--reset-tablet');
+var cta = document.querySelector('.container--cta-reset-tablet');
 
 header.classList.remove('page-header--nojs');
 header.classList.remove('page-header--no-opacity');
 navigation.classList.add('navigation--toggle');
 menuButton.classList.remove('page-header__close--nojs');
-if (cta) {cta.classList.remove('container__cta--reset-tablet--nojs');}
+if (cta) {cta.classList.remove('container--cta-reset-tablet--nojs');}
 
 navToggle.addEventListener('click', function () {
   navigation.classList.toggle('navigation--toggle');
